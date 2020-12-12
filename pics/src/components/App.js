a4334574ac0f9914501aeb7e19d5d2efb1799a5d
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-12-12 09:39:59 -0500

    Desc

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
         return (
             <div className="ui container" style={{marginTop: "10px"}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
-                Found: {this.state.images.length} images
+                Found: {this.state.images.length} imagess
                 <ImageList images={this.state.images}/>
             </div>
         )
